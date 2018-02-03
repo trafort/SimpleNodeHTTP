b8d5e4f6d80af6bc8719f58b75121c1cfb7e44d4
trafort <34495933+trafort@users.noreply.github.com>
2018-02-03 16:33:30 -0500

    Add Html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end(Helllo! Welcome To your own Test Http Server');
+res.writeHead(200, {"Content-Type": "text/html"});
+res.write('<!DOCTYPE html>'+
+'<html>'+
+' <head>'+
+' <meta charset="utf-8" />'+
+' <title>My Node.js page!</title>'+
+' </head>'+ 
+' <body>'+
+' <p>Here is a paragraph of <strong>HTML</strong>!</p>'+
+' </body>'+
+'</html>');
+res.end();
 });
-server.listen(1012);
+server.listen(8080);
